ea54f7ce5e5f767005a15064521eede36e875dca
mnoomnoo <mnoomnoo@users.noreply.github.com>
2021-07-05 20:50:36 -0700

    update to printConsole
    added output to the CS console

--- src/Common.h
+++ src/Common.h
@@ -3,12 +3,21 @@
 #include <sstream>
 #include <iostream>
 
-const static wchar_t* PROGRAM_VERSION = L"1.3.21.5.10"; // Major-Minor-Year-Month-Day
+const static wchar_t* PROGRAM_VERSION = L"1.3.21.7.5"; // Major-Minor-Year-Month-Day
 
-static void __PrintToConsole( const std::wstringstream& ss )
+#define __PRINT_TO_VS_OUTPUT_WINDOW
+#ifdef __PRINT_TO_VS_OUTPUT_WINDOW
+	#include "Windows.h"
+#endif
+
+
+inline void __PrintToConsole( const std::wstringstream& ss )
 {
 	std::wcout << ss.str().c_str();
 	std::wcout.flush();
+#ifdef __PRINT_TO_VS_OUTPUT_WINDOW
+	OutputDebugString( ss.str().c_str() );
+#endif
 }
 
 #define PrintToConsole( s ) __PrintToConsole( std::wstringstream() << s )
